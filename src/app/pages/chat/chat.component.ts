46da4228017f37336c8a328f1a3c625798661143
Yehor <egorbut300016@gmail.com>
2024-07-19 23:19:45 +0300

    Update frontend

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -31,25 +31,24 @@ import {TokenService} from "../../service/token.service";
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent implements OnInit, AfterViewChecked {
-  message: string = ''
-  messages: any[] = []
-  rooms: ChatRoom[] = []
+  message: string = '';
+  messages: any[] = [];
+  rooms: ChatRoom[] = [];
   user2Name: string = '';
   chat: boolean = false;
   filteredRooms = [...this.rooms];
-  searchChatText: string = ''
-  username: string = ''
-  anotherUsername: string = ''
-  currentUserId: string = ''
-  anotherUserStatus: string = 'offline'
+  searchChatText: string = '';
+  username: string = '';
+  anotherUsername: string = '';
+  currentUserId: string = '';
+  anotherUserStatus: string = 'Online';
   private room: any;
-  private stompClient: any
-  private currentSubscription: any
+  private stompClient: any;
+  private currentSubscription: any;
   private userStatusSubscription: any;
   private token: string = '';
   selectedStatus: string = 'Online';
   @ViewChild('scrollMe') private myScrollContainer!: ElementRef<HTMLDivElement>;
-
 
   constructor(private themeService: ThemeServiceService,
               private router: Router,
@@ -60,70 +59,74 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   }
 
   ngOnInit(): void {
-    const token = this.tokenService.getToken()
-    if(token == null) {
-      this.router.navigate(['/login'])
+    const token = this.tokenService.getToken();
+    if (token == null) {
+      this.router.navigate(['/login']);
     }
 
-    this.currentUserId = this.tokenService.decodeToken(token).id
+    this.currentUserId = this.tokenService.decodeToken(token).id;
     this.themeService.setTheme(this.themeService.getTheme());
-    this.username = this.tokenService.decodeToken(token).username
+    this.username = this.tokenService.decodeToken(token).username;
 
-    this.updateRooms()
+    this.updateRooms();
     this.initConnection();
     this.join();
   }
 
   private initConnection() {
-    const URL = '//localhost:8080/ws'
+    const URL = '//localhost:8080/ws';
     const socket = new SockJS(URL);
     this.stompClient = Stomp.over(socket);
   }
+
   private join() {
     this.stompClient.connect({
       'Authorization': 'Bearer ' + this.token
     }, () => {
       this.chatService.changeUserStatus(this.currentUserId, "Online").subscribe(() => {
-        this.sendUserStatus("Online")
-      })
+        this.sendUserStatus("Online");
+      });
     }, (error: any) => {
       this.chatService.changeUserStatus(this.currentUserId, "Offline").subscribe(() => {
-        this.sendUserStatus("Offline")
-      })
-    })
+        this.sendUserStatus("Offline");
+      });
+    });
 
     this.stompClient.ws.onclose = () => {
       this.chatService.changeUserStatus(this.currentUserId, "Offline").subscribe(() => {
-        this.sendUserStatus("Offline")
-      })
-    }
+        this.sendUserStatus("Offline");
+      });
+    };
   }
+
   public sendUserStatus(status: string) {
     const statusMessage = {
       userId: this.currentUserId,
       status: status
-    }
-    this.stompClient.send("/app/status", {}, JSON.stringify(statusMessage))
+    };
+    this.stompClient.send("/app/status", {}, JSON.stringify(statusMessage));
   }
 
   onChangeStatus(event: any) {
     this.chatService.changeUserStatus(this.currentUserId, this.selectedStatus).subscribe(() => {
       this.sendUserStatus(this.selectedStatus);
-    })
+    });
   }
+
   public manageChat(room: any) {
     this.chatService.deleteChatRoom(room.id).subscribe((result: any) => {
-      console.log(result)
-      this.updateRooms()
-    })
+      console.log(result);
+      this.updateRooms();
+    });
   }
+
   public sendRoomMessage() {
     const currentDate = new Date();
     const hours = currentDate.getHours().toString().padStart(2, '0');
     const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    if(this.message.trim().length == 0) {
-      alert('Your message must contain text')
-      return
+    if (this.message.trim().length === 0) {
+      alert('Your message must contain text');
+      return;
     }
     const messageJSON = {
       content: this.message,
@@ -131,59 +134,63 @@ export class ChatComponent implements OnInit, AfterViewChecked {
       senderId: this.currentUserId,
       chatRoomId: this.room.id,
       messageStatus: 'DELIVERED'
-    }
+    };
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-    })
+    });
     this.stompClient.send('/app/chat', { headers }, JSON.stringify(messageJSON));
-    this.message = ''
+    this.message = '';
   }
 
   openChat(room: any) {
-    this.chat = true
+    this.chat = true;
     const token = this.tokenService.getToken();
     const currentUser = this.tokenService.decodeToken(token).username;
-    this.message = ''
+    this.message = '';
 
-    if (room.user1Name === currentUser) {
-      this.anotherUsername = room.user2Name;
-      this.subscribeToUserStatusUpdates(room.user2Id)
-    } else {
-      this.anotherUsername = room.user1Name;
-      this.subscribeToUserStatusUpdates(room.user1Id)
-    }
-    this.messages = []
-    this.room = room
-    console.log(room)
-    if(this.currentSubscription) {
+    if (this.currentSubscription) {
       this.currentSubscription.unsubscribe();
     }
     if (this.userStatusSubscription) {
-      this.userStatusSubscription.unsubscribe()
+      this.userStatusSubscription.unsubscribe();
     }
-    this.user2Name = room.user2Name
-    this.showRoomMessages(room.id)
-    console.log("SASKO " + room.id)
-    this.currentSubscription = this.stompClient.subscribe('/user/'+ room.id + '/queue/messages',(messages: any) => {
-      this.messages.push(JSON.parse(messages.body))
-    })
 
+    if (room.user1Name === currentUser) {
+      this.anotherUsername = room.user2Name;
+      this.subscribeToUserStatusUpdates(room.user2Id);
+    } else {
+      this.anotherUsername = room.user1Name;
+      this.subscribeToUserStatusUpdates(room.user1Id);
+    }
+    this.messages = [];
+    this.room = room;
+    console.log(room);
+    this.user2Name = room.user2Name;
+    this.showRoomMessages(room.id);
+    console.log("SASKO " + room.id);
+    this.currentSubscription = this.stompClient.subscribe('/user/' + room.id + '/queue/messages', (messages: any) => {
+      this.messages.push(JSON.parse(messages.body));
+    });
   }
+
   public subscribeToUserStatusUpdates(userId: string) {
     this.userStatusSubscription = this.stompClient.subscribe("/topic/userStatus/" + userId, (message: any) => {
-      const statusUpdate = JSON.parse(message.body)
+      const statusUpdate = JSON.parse(message.body);
       this.anotherUserStatus = statusUpdate.status;
       console.log(`User ${statusUpdate.userId} is now ${statusUpdate.status}`);
-    })
+    });
   }
+
   searchChat() {
     this.filteredRooms = this.rooms.filter((room) =>
       room.user2Name.toLowerCase().includes(this.searchChatText.toLowerCase())
     );
   }
+
   ngAfterViewChecked(): void {
-    this.scrollToBottom()
+    this.scrollToBottom();
   }
+
   private showRoomMessages(roomId: string) {
     this.chatService.getRoomMessages(roomId, this.currentUserId).subscribe((roomMessages) => {
       console.log(roomMessages);
@@ -197,15 +204,16 @@ export class ChatComponent implements OnInit, AfterViewChecked {
       width: '1200px',
       height: '500px',
       data: {}
-    })
+    });
 
     dialogRef.componentInstance.openChatEvent.subscribe((user2Id: string) => {
       this.chatService.createChatRoom(user2Id).subscribe((room: any) => {
-        this.openChat(room)
-        this.updateRooms()
-      })
-    })
+        this.openChat(room);
+        this.updateRooms();
+      });
+    });
   }
+
   private updateRooms() {
     this.chatService.getUserRooms().subscribe((rooms: any) => {
       const token = this.tokenService.getToken();
@@ -226,12 +234,11 @@ export class ChatComponent implements OnInit, AfterViewChecked {
 
   scrollToBottom(): void {
     try {
-      if(this.myScrollContainer) {
+      if (this.myScrollContainer) {
         this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 }
-
